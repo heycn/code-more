39326cc3c5b845421344b5c0e57664a8bf240f19
heycn <heycn@foxmail.com>
2023-05-11 15:58:21 +0800

    不用 ...操作符，使用 slice 代替处理

--- 004_Bind/src/index.js
+++ 004_Bind/src/index.js
@@ -1,11 +1,14 @@
-function bind(asThis, ...args) {
+var slice = Array.prototype.slice
+function bind(asThis) {
   // this 就是函数
+  var args1 = slice.call(arguments, 1)
   var fn = this
   if (typeof fn !== 'function') {
     throw new TypeError('bind 必须调用在函数上')
   }
-  return function (...args2) {
-    return fn.call(asThis, ...args, ...args2)
+  return function () {
+    var args2 = slice.call(arguments, 0)
+    return fn.apply(asThis, args1.concat(args2))
   }
 }
 
