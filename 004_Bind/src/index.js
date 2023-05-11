b698794c19e54b207a764a62631c2d6831a9bd4b
heycn <heycn@foxmail.com>
2023-05-11 15:51:26 +0800

    加一句错误提示

--- 004_Bind/src/index.js
+++ 004_Bind/src/index.js
@@ -1,6 +1,9 @@
 function bind(asThis, ...args) {
   // this 就是函数
   var fn = this
+  if (typeof fn !== 'function') {
+    throw new TypeError('bind 必须调用在函数上')
+  }
   return function (...args2) {
     return fn.call(asThis, ...args, ...args2)
   }
