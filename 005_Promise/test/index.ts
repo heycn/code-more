7ee93101be6b9f907808ad79e8aed8b5de298e39
heycn <heycn@foxmail.com>
2023-05-14 00:08:08 +0800

    使用 sinon 测试函数，并且优化了测试代码

--- 005_Promise/test/index.ts
+++ 005_Promise/test/index.ts
@@ -1,7 +1,13 @@
 // @ts-nocheck
+import chai from 'chai'
 import { assert } from 'chai'
 import { describe } from 'mocha'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+chai.use(sinonChai)
+
 import Promise from '../src'
+
 
 describe("Promise", () => {
   it("是一个类", () => {
@@ -9,53 +15,37 @@ describe("Promise", () => {
     assert.isObject(Promise.prototype)
   })
   it("new Promise() 如果接受的不是一个函数就报错", () => {
-    assert.throw(() => {
-      // @ts-ignore
-      new Promise()
-    })
-    assert.throw(() => {
-      // @ts-ignore
-      new Promise(1)
-    })
-    assert.throw(() => {
-      // @ts-ignore
-      new Promise('1')
-    })
-    assert.throw(() => {
-      // @ts-ignore
-      new Promise(false)
-    })
+    assert.throw(() => new Promise())
+    assert.throw(() => new Promise(1))
+    assert.throw(() => new Promise('1'))
+    assert.throw(() => new Promise(false))
   })
   it("new Promise(fn) 会生成一个对象，对象有 then 方法", () => {
     const promise = new Promise(() => {})
     assert.isFunction(promise.then)
   })
   it("new Promise(fn) 中的 fn 是立即执行的", () => {
-    let called = false
-    const promise = new Promise(() => {
-      called = true
-    })
-    // @ts-ignore
-    assert(called === true)
+    const fn = sinon.fake()
+    new Promise(fn)
+    assert(fn.called)
   })
-  it("new Promise(fn) 中的 fn 接受两个函数作为参数", () => {
-    const promise = new Promise((resolve, reject) => {
+  it("new Promise(fn) 中的 fn 接受两个函数作为参数", done => {
+    new Promise((resolve, reject) => {
       assert.isFunction(resolve)
       assert.isFunction(reject)
+      done()
     })
   })
   it("promise.then 中的 success 会在 resolve 被调用的时候执行", done => {
-    let called = false
+    const success = sinon.fake()
     const promise = new Promise((resolve, reject) => {
-      assert(called === false)
+      assert.isFalse(success.called)
       resolve()
       setTimeout(() => {
-        assert(called === true)
+        assert.isTrue(success.called)
         done()
       })
     })
-    promise.then(() => {
-      called = true
-    })
+    promise.then(success)
   })
 })
