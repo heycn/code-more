4cc889ac6aa02987db3aa611c30e24923bdc1d0b
heycn <heycn@foxmail.com>
2023-05-13 22:41:00 +0800

    Promise 接受一个函数

--- 005_Promise/src/index.ts
+++ 005_Promise/src/index.ts
@@ -1,3 +1,9 @@
-class Promise2 {}
+class Promise2 {
+  constructor(fn: Function) {
+    if (typeof fn !== "function") {
+      throw new Error('我只接受一个函数！')
+    }
+  }
+}
 
 export default Promise2
--- 005_Promise/test/index.ts
+++ 005_Promise/test/index.ts
@@ -7,4 +7,22 @@ describe("Promise", () => {
     assert.isFunction(Promise)
     assert.isObject(Promise.prototype)
   })
+  it("new Promise() 如果接受的不是一个函数就报错", () => {
+    assert.throw(() => {
+      // @ts-ignore
+      new Promise()
+    })
+    assert.throw(() => {
+      // @ts-ignore
+      new Promise(1)
+    })
+    assert.throw(() => {
+      // @ts-ignore
+      new Promise('1')
+    })
+    assert.throw(() => {
+      // @ts-ignore
+      new Promise(false)
+    })
+  })
 })
